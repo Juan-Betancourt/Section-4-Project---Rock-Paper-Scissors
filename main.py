676ab16c518b32a5f79584913512a83f9320bf54
Juan Betancourt <juancbetancourt11@gmail.com>
2022-04-20 02:33:45 +0000

    Correcting output and invalid numbers.

--- main.py
+++ main.py
@@ -32,39 +32,39 @@ rps_game = [rock, paper, scissors]
 
 player_selection = int(input("What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors. \n"))
 
-player_hand_pick = (rps_game[player_selection])
-print(player_hand_pick)
-
-if player_selection == 0:
-  print("You selected Rock")
-elif player_selection == 1:
-  print("You Selected Paper")
-else:
-  print("You selected Scissors")
-
-computer_output = random.randint(0, 2)
-
-if computer_output == 0:
-  print(rock)
-  print("Computer Selects Rock")
-elif computer_output == 1:
-  print(paper)
-  print("Computer selects Paper")
-else:
-  print(scissors)
-  print("Computer Selects Scissors\n")
-
-
-if player_selection == 0 and computer_output == 2:
-  print("You win!")
-elif computer_output == 0 and player_selection == 2:
-  print("You lose!")
-elif computer_output > player_selection:
-  print("You lose!")
-elif player_selection > computer_output:
-  print("You win!")
-elif computer_output == player_selection:
-  print("It's a draw!")
-else:
+if player_selection >= 3 or player_selection < 0:
   print("You have an invalid number!")
+else:
+  player_hand_pick = (rps_game[player_selection])
+  print(player_hand_pick)
   
+  if player_selection == 0:
+    print("You selected Rock")
+  elif player_selection == 1:
+    print("You Selected Paper")
+  else:
+    print("You selected Scissors")
+  
+  computer_output = random.randint(0, 2)
+  
+  if computer_output == 0:
+    print(rock)
+    print("Computer Selects Rock")
+  elif computer_output == 1:
+    print(paper)
+    print("Computer selects Paper")
+  else:
+    print(scissors)
+    print("Computer Selects Scissors\n")
+  
+  if player_selection == 0 and computer_output == 2:
+    print("You win!")
+  elif computer_output == 0 and player_selection == 2:
+    print("You lose!")
+  elif computer_output > player_selection:
+    print("You lose!")
+  elif player_selection > computer_output:
+    print("You win!")
+  elif computer_output == player_selection:
+    print("It's a draw!")
+    
